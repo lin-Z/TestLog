3ca4a52e46ccdb9f7f4878c109bed85a64b1a92b
lin-Z <1312381829@qq.com>
2017-03-10 16:38:59 +0800

    添加类
    添加类

---
+++ TestLog/Classes/SGSObject.h
@@ -0,0 +1,15 @@
+//
+//  SGSObject.h
+//  TestLog
+//
+//  Created by 张淋 on 2017/3/10.
+//  Copyright © 2017年 lin-Z. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SGSObject : NSObject
+
+- (void)eat;
+
+@end
---
+++ TestLog/Classes/SGSObject.m
@@ -0,0 +1,17 @@
+//
+//  SGSObject.m
+//  TestLog
+//
+//  Created by 张淋 on 2017/3/10.
+//  Copyright © 2017年 lin-Z. All rights reserved.
+//
+
+#import "SGSObject.h"
+
+@implementation SGSObject
+
+- (void)eat {
+    NSLog(@"eating...");
+}
+
+@end
